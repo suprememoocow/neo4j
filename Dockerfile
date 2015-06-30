fcde8d5ad086881bbe6ffa385486283f632fb3df
Andrew Newdigate <andrew@troupe.co>
2015-06-30 15:53:33 +0100

    Work on getting Neo4J HA availability working in a cluster

--- Dockerfile
+++ Dockerfile
@@ -1,35 +1,30 @@
 ## Neo4J dependency: java
 ## get java from official repo
-from java:latest
-maintainer Tiago Pires, tandrepires@gmail.com
+FROM java:latest
+MAINTAINER Tiago Pires, tandrepires@gmail.com
 
 ## install neo4j according to http://www.neo4j.org/download/linux
 # Import neo4j signing key
 # Create an apt sources.list file
 # Find out about the files in neo4j repo ; install neo4j community edition
 
-run wget -O - http://debian.neo4j.org/neotechnology.gpg.key | apt-key add - && \
+RUN wget -O - http://debian.neo4j.org/neotechnology.gpg.key | apt-key add - && \
     echo 'deb http://debian.neo4j.org/repo stable/' > /etc/apt/sources.list.d/neo4j.list && \
     apt-get update ; apt-get install neo4j -y
 
-## add launcher and set execute property
-## clean sources
-## turn on indexing: http://chrislarson.me/blog/install-neo4j-graph-database-ubuntu
-## enable neo4j indexing, and set indexable keys to name,age
-# enable shell server on all network interfaces
+ADD launch.sh /
+RUN chmod +x /launch.sh
 
-add launch.sh /
-run chmod +x /launch.sh && \
-    apt-get clean && \
-    sed -i "s|#node_auto_indexing|node_auto_indexing|g" /var/lib/neo4j/conf/neo4j.properties && \
-    sed -i "s|#node_keys_indexable|node_keys_indexable|g" /var/lib/neo4j/conf/neo4j.properties && \ 
-    echo "remote_shell_host=0.0.0.0" >> /var/lib/neo4j/conf/neo4j.properties
+RUN apt-get clean
+RUN rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
 # expose REST and shell server ports
-expose 7474
-expose 1337
+EXPOSE 7474
+EXPOSE 1337
+EXPOSE 5001
+EXPOSE 6001
 
-workdir /
+WORKDIR /
 
 ## entrypoint
-cmd ["/bin/bash", "-c", "/launch.sh"]
+CMD ["/bin/bash", "-c", "/launch.sh"]
--- launch.sh
+++ launch.sh
@@ -1,17 +1,69 @@
 #!/bin/bash
+set -e
+set -x
 
 NEO4J_HOME=/var/lib/neo4j
+NEO4J_SERVER_PROPS=$NEO4J_HOME/conf/neo4j-server.properties
+NEO4J_PROPS=$NEO4J_HOME/conf/neo4j.properties
 
-sed -i "s|#org.neo4j.server.webserver.address=0.0.0.0|org.neo4j.server.webserver.address=$HOSTNAME|g" $NEO4J_HOME/conf/neo4j-server.properties
+cat <<EOD > $NEO4J_PROPS
+# node_auto_indexing=true
+# node_keys_indexable=true
+allow_store_upgrade=true
+#dbms.pagecache.memory=10g
+cache_type=soft
+wrapper.java.initmemory=12000
+wrapper.java.maxmemory=12000
+keep_logical_logs=false
+remote_shell_enabled=true
+remote_shell_host=$HOSTNAME
+EOD
+
+cat <<EOD > $NEO4J_SERVER_PROPS
+org.neo4j.server.database.location=/opt/data/graph.db
+org.neo4j.server.db.tuning.properties=conf/neo4j.properties
+org.neo4j.server.webserver.address=$HOSTNAME
+dbms.security.auth_enabled=false
+org.neo4j.server.webserver.port=7474
+org.neo4j.server.webserver.https.enabled=true
+org.neo4j.server.webserver.https.port=7473
+org.neo4j.server.webserver.https.cert.location=conf/ssl/snakeoil.cert
+org.neo4j.server.webserver.https.key.location=conf/ssl/snakeoil.key
+org.neo4j.server.webserver.https.keystore.location=data/keystore
+# org.neo4j.server.thirdparty_jaxrs_classes=extension=/service
+org.neo4j.server.http.log.enabled=false
+org.neo4j.server.http.log.config=conf/neo4j-http-logging.xml
+org.neo4j.server.webadmin.rrdb.location=/opt/data/graph.db/rrd
+EOD
+
+# Add HA parameters
+if [[ -n $NEO4J_HA ]]; then
+  cat <<EOD >> $NEO4J_PROPS
+ha.server_id=$NEO4J_HA_SERVER_ID
+# ha.initial_hosts=
+ha.cluster_server=$HOSTNAME:5001
+ha.server=$HOSTNAME:6001
+EOD
+fi
+
+  cat <<EOD >> $NEO4J_SERVER_PROPS
+org.neo4j.server.database.mode=HA
+EOD
+
+echo $NEO4J_SERVER_PROPS
+cat $NEO4J_SERVER_PROPS
+echo $NEO4J_PROPS
+cat $NEO4J_PROPS
+
+set
 
 # doing this conditionally in case there is already a limit higher than what
 # we're setting here. neo4j recommends at least 40000.
-# 
+#
 # (http://neo4j.com/docs/1.6.2/configuration-linux-notes.html#_setting_the_number_of_open_files)
 limit=`ulimit -n`
 if [ "$limit" -lt 65536 ]; then
     ulimit -n 65536;
 fi
-    
-.$NEO4J_HOME/bin/neo4j console
 
+.$NEO4J_HOME/bin/neo4j console
